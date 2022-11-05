4be3ede7006af8eca66e7a28dad6ae1924b1adcc
ShravanMCAnu <bskmc850@gmail.com>
2022-11-05 16:14:57 +0530

    Add project files.

---
+++ Test1_ShravanKumar_Project8/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_ShravanKumar_Project8
+{
+    public class PrimeNumber
+    {
+        public int iterator;
+        bool prime = true;
+        public PrimeNumber(int number)
+        {
+            for (int j = 2; j <= number / 2; j++)
+            {
+                if (number % j == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime)
+            {
+                Console.WriteLine("prime");
+            }
+            else
+            {
+                Console.WriteLine("Not prime");
+            }
+        }
+    }
+}
---
+++ Test1_ShravanKumar_Project8/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test1_ShravanKumar_Project8;
+Console.WriteLine("Enter a number:  ");
+int number=Convert.ToInt32(Console.ReadLine());
+PrimeNumber ObjPrimeNumber = new PrimeNumber(number);
